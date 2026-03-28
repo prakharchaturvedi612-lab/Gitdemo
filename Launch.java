59ab667bc946efce4c2049d204e677ae19b133f0
Prakhar Chaturvedi <prakharchaturvedi612@gmail.com>
2026-03-28 12:44:29 +0530

    Modified after cloning

--- Launch.java
+++ Launch.java
@@ -4,4 +4,12 @@ class Launch
 {
 System.out.println("Exploring Git and GitHub ) ; 
 }
+
+      public static void fun(){
+           int a = 10 ;
+           int b = 20 ;
+	   sum =  a+b ;
+           System.out.println(sum) ; }
+
+
 }
